c3a33a296c7019e89e3e0ee2fe72f0a4f3bc949b
MakerSquare Student <students@themakersquare.com>
2013-09-24 14:32:44 -0500

    Make show_gladiator more awesome

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,7 @@
 class VideosController < ApplicationController
   def show_gladiator
+    @title = "Gladiator 'Are You Not Entertained' Scene"
+    @description = "Russel Crowe is a boss in this video"
   end
 
   def show_air_force_one
