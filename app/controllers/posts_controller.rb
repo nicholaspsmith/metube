9d4b8b163ea2a4e1adb9fef57cfaba01ddbed6ed
MakerSquare Student <students@themakersquare.com>
2013-09-24 14:24:14 -0500

    Add cities

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,9 @@
+class PostsController < ApplicationController
+  def show_jersey_city
+    
+  end
+
+  def show_el_paso
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Metube::Application.routes.draw do
   get "/videos/air_force_one" => "videos#show_air_force_one"
   get "/videos/nelly" => "videos#show_nelly"
   get "/videos/dream_theater" => "videos#show_dream_theater"
+
+  get "/posts/jersey_city" => "posts#show_jersey_city"
+  get "/posts/el_paso" => "posts#show_el_paso"
 end
 
