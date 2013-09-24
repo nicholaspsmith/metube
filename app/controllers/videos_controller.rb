86de2574c69210917915375c450b1e6bdd45c9a9
MakerSquare Student <students@themakersquare.com>
2013-09-24 14:07:42 -0500

    add awesome videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,13 @@
 class VideosController < ApplicationController
   def show_gladiator
   end
+
+  def show_air_force_one
+  end
+
+  def show_nelly
+  end
+
+  def show_dream_theater
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Metube::Application.routes.draw do
   get "/videos/gladiator" => "videos#show_gladiator"
+  get "/videos/air_force_one" => "videos#show_air_force_one"
+  get "/videos/nelly" => "videos#show_nelly"
+  get "/videos/dream_theater" => "videos#show_dream_theater"
 end
+
